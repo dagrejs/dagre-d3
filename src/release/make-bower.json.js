a7705901d801236689f97db0e2d787ac80c28101
Chris Pettitt <cpettitt@gmail.com>
2014-09-30 06:45:02 -0700

    Consistent use of double quotes

--- src/release/make-bower.json.js
+++ src/release/make-bower.json.js
@@ -1,14 +1,14 @@
 // Renders the bower.json template and prints it to stdout
 
 var template = {
-  name: 'dagre-d3',
-  version: require('../../package.json').version,
-  main: ['js/dagre-d3.js', 'js/dagre-d3.min.js'],
+  name: "dagre-d3",
+  version: require("../../package.json").version,
+  main: ["js/dagre-d3.js", "js/dagre-d3.min.js"],
   ignore: [
-    'README.md'
+    "README.md"
   ],
   dependencies: {
-    'd3': '~3.3.8'
+    "d3": "~3.3.8"
   }
 };
 
