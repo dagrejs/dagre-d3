aa51af2e50aeb0c6750e1e7adbf6505adc8e5d51
Chris Pettitt <cpettitt@gmail.com>
2014-10-18 16:02:39 -0700

    Initial work on code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -31,14 +31,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'build/dagre-d3.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+      type: 'html',
+      dir: 'build/cov'
+    },
 
     // web server port
     port: 9876,
