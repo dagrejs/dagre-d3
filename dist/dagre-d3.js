cae18a9e0bf6b9597f64baac7e407cd788f9d8e1
Chris Pettitt <cpettitt@gmail.com>
2017-12-27 12:10:55 -0700

    Prep v0.5.2 release

--- dist/dagre-d3.js
+++ dist/dagre-d3.js
@@ -1192,7 +1192,7 @@ function applyTransition(selection, g) {
 }
 
 },{"./lodash":21}],28:[function(require,module,exports){
-module.exports = "0.5.1";
+module.exports = "0.5.2";
 
 },{}],29:[function(require,module,exports){
 /*
