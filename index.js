0d0fc888ad5083de13453e1eecc5c71f7dd847a4
Andrey Nikitenko <andrey.nikitenko@mustardlabs.com>
2018-02-17 19:15:49 +0200

    webpack compatible

--- index.js
+++ index.js
@@ -21,8 +21,8 @@
  * THE SOFTWARE.
  */
 module.exports =  {
-  graphlib: require("./lib/graphlib"),
-  dagre: require("./lib/dagre"),
+  graphlib: require("graphlib"),
+  dagre: require("dagre"),
   intersect: require("./lib/intersect"),
   render: require("./lib/render"),
   util: require("./lib/util"),
