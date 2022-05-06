136a27c6dd8738bce405fb7665fe6f9ad4213d19
Tim Paine <t.paine154@gmail.com>
2022-05-05 23:05:23 -0400

    setup github actions

--- karma.core.conf.js
+++ karma.core.conf.js
@@ -72,7 +72,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
